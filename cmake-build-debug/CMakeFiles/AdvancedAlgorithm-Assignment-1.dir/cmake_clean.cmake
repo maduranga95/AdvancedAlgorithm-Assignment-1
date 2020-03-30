ba0f2fdc2ad520a22de40e9710b344946e75a949
Uditha Maduranga <maduranga95@gmail.com>
2020-03-30 14:49:11 +0530

    Added CMakeLists.txt

---
+++ cmake-build-debug/CMakeFiles/AdvancedAlgorithm-Assignment-1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AdvancedAlgorithm-Assignment-1.exe"
+  "AdvancedAlgorithm-Assignment-1.exe.manifest"
+  "AdvancedAlgorithm-Assignment-1.lib"
+  "AdvancedAlgorithm-Assignment-1.pdb"
+  "CMakeFiles/AdvancedAlgorithm-Assignment-1.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AdvancedAlgorithm-Assignment-1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
