2b68ea106fbe43d9e2da689e8244c1a873e92b61
Uditha Maduranga <maduranga95@gmail.com>
2020-03-30 17:32:02 +0530

    Correction

--- main.cpp
+++ main.cpp
@@ -12,134 +12,124 @@ using namespace std;
 using namespace std::chrono;
 
 // Intertions in to BST
-BST* insertToBST(ifstream& f, string x){
+BST* BSTInsertion(ifstream& f, string s){
     BST *bst = new BST();
     auto start = high_resolution_clock::now();
-    while (getline(f, x, '\n')) {
-        bst->put(stoi(x),stoi(x)  );
+    while (getline(f, s, '\n')) {
+        bst->put(stoi(s),stoi(s)  );
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Insertion time of BST : " << duration.count() << " microseconds"<< endl;
     cout << "Height of BST : " << bst->height() << endl;
-    //cout << "Number of nodes in BST : " << bst->size() << endl;
     return bst;
 }
 
 // Inserions in to Splay tree
-SplayTree* insertToSplayTree(ifstream& f, string x){
+SplayTree* SplayTreeInsertion(ifstream& f, string s){
     SplayTree* splayTree = new  SplayTree();
     auto start = high_resolution_clock::now();
-    while (getline(f, x, '\n')) {
-        splayTree->put(stoi(x),stoi(x) );
+    while (getline(f, s, '\n')) {
+        splayTree->put(stoi(s),stoi(s) );
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Insertion time of Splay Tree : " << duration.count() << " microseconds"<< endl;
     cout << "Height of Splay Tree : " << splayTree->height() << endl;
-    //cout << "Number of nodes in Splay Tree : " << splayTree->size() << endl;
     return  splayTree;
 
 }
 
 // Insertions in to Red-black tree
-RBTree*  insertToRBTree(ifstream& f, string x){
+RBTree*  RBTreeInsertion(ifstream& f, string s){
     RBTree* rbTree = new  RBTree();
     auto start = high_resolution_clock::now();
-    while (getline(f, x, '\n')) {
-        rbTree->put(stoi(x),stoi(x) );
+    while (getline(f, s, '\n')) {
+        rbTree->put(stoi(s),stoi(s) );
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Insertion time of Red-Black Tree : " << duration.count() << " microseconds"<< endl;
     cout << "Height of Red-Black Tree : " << rbTree->height() << endl;
-    //cout << "Number of nodes in Red-Black Tree : " << rbTree->size() << endl;
     return rbTree;
 
 }
 
-// Deletions from BST
-void deleteFromBST(BST* bst, ifstream& f, string x){
-    auto start = high_resolution_clock::now();
-    while (getline(f, x, '\n')) {
-        bst->del(stoi(x));
-    }
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout << "Deletion time of BST : " << duration.count() << " microseconds"<< endl;
-    cout << "Height of BST after deletion : " << bst->height() << endl;
-    //cout << "Number of nodes in BST after deletion : " << bst->size() << endl;
-
-}
-
-// Deletions from Splay tree
-void deleteFromSplayTree(SplayTree* splayTree,ifstream& f, string x){
-    auto start = high_resolution_clock::now();
-    while (getline(f, x, '\n')) {
-        splayTree->del(stoi(x));
-    }
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout << "Deletion time of Splay Tree : " << duration.count() << " microseconds"<< endl;
-    cout << "Height of Splay Tree after deletion : " << splayTree->height() << endl;
-    //cout << "Number of nodes in Splay Tree after deletion : " << splayTree->size() << endl;
-
-}
-
-// Deletions from Red-black tree
-void deleteFromRBTree(RBTree* rbTree, ifstream& f, string x){
-    auto start = high_resolution_clock::now();
-    while (getline(f, x, '\n')) {
-        rbTree->del(stoi(x));
-    }
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout << "Deletion time of Red-Black Tree : " << duration.count() << " microseconds"<< endl;
-    cout << "Height of Red-Black Tree after deletion : " << rbTree->height() << endl;
-    //cout << "Number of nodes in Red-Black Tree after deletion : " << rbTree->size() << endl;
-
-}
-
-
 // Search from BST
-void searchBST(BST* bst, string x, ifstream& f){
+void BSTSearch(BST* bst, string s, ifstream& f){
     auto start = high_resolution_clock::now();
-    while (getline(f, x, '\n')) {
-        bst->get(stoi(x));
+    while (getline(f, s, '\n')) {
+        bst->get(stoi(s));
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Search time of BST : " << duration.count() << " microseconds"<< endl;
     cout << "Height of BST after search : " << bst->height() << endl;
-    //cout << "Number of nodes in BST after search : " << bst->size() << endl;
 
 }
 
 // Search from Splay tree
-void searchSplayTree(SplayTree* splayTree,  string x, ifstream& f){
+void SplayTreeSearch(SplayTree* splayTree,  string s, ifstream& f){
     auto start = high_resolution_clock::now();
-    while (getline(f, x, '\n')) {
-        splayTree->contains(stoi(x));
+    while (getline(f, s, '\n')) {
+        splayTree->contains(stoi(s));
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Search time of Splay Tree : " << duration.count() << " microseconds"<< endl;
     cout << "Height of Splay Tree after search : " << splayTree->height() << endl;
-    //cout << "Number of nodes in Splay Tree after search : " << splayTree->size() << endl;
 
 }
 
 // Search from Red-black tree
-void searchRBTree(RBTree* rbTree, string x, ifstream& f){
+void RBTreeSearch(RBTree* rbTree, string s, ifstream& f){
     auto start = high_resolution_clock::now();
-    while (getline(f, x, '\n')) {
-        rbTree->get(stoi(x));
+    while (getline(f, s, '\n')) {
+        rbTree->get(stoi(s));
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Search time of Red-Black Tree : " << duration.count() << " microseconds"<< endl;
     cout << "Height of Red-Black Tree after search : " << rbTree->height() << endl;
-    //cout << "Number of nodes in Red-Black Tree after search : " << rbTree->size() << endl;
+
+}
+
+// Deletions from BST
+void BSTDeletion(BST* bst, ifstream& f, string s){
+    auto start = high_resolution_clock::now();
+    while (getline(f, s, '\n')) {
+        bst->del(stoi(s));
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Deletion time of BST : " << duration.count() << " microseconds"<< endl;
+    cout << "Height of BST after deletion : " << bst->height() << endl;
+
+}
+
+// Deletions from Splay tree
+void SplayTreeDeletion(SplayTree* splayTree,ifstream& f, string s){
+    auto start = high_resolution_clock::now();
+    while (getline(f, s, '\n')) {
+        splayTree->del(stoi(s));
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Deletion time of Splay Tree : " << duration.count() << " microseconds"<< endl;
+    cout << "Height of Splay Tree after deletion : " << splayTree->height() << endl;
+
+}
+
+// Deletions from Red-black tree
+void RBTreeDeletion(RBTree* rbTree, ifstream& f, string s){
+    auto start = high_resolution_clock::now();
+    while (getline(f, s, '\n')) {
+        rbTree->del(stoi(s));
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Deletion time of Red-Black Tree : " << duration.count() << " microseconds"<< endl;
+    cout << "Height of Red-Black Tree after deletion : " << rbTree->height() << endl;
 
 }
 
@@ -153,46 +143,41 @@ int main() {
     ifstream data_2("\\data\\data_2.txt");
     ifstream search_data("\\data\\search_data.txt");
     ifstream delete_data("\\data\\delete_data.txt");
-    string x;
+    string s;
 
+    // Here you can uncomment the insertion line to insert to each tree one at a time
+    // To do search operation, you can uncomment insertion and search lines in each tree one at a time
+    // To do delete operation, you can uncomment insertion and delete lines in each tree one at a time
 
     // BST with dataset_1
-    /*BST *bst1 = insertToBST(data_1,x);
-    searchBST(bst1,x,search_data);
-    deleteFromBST(bst1,delete_data,x);
-    cout << '\n';*/
+    /*BST *bst_d1 = BSTInsertion(data_1,s);
+    BSTSearch(bst_d1,s,search_data);
+    BSTDeletion(bst_d1,delete_data,s);*/
 
     // BST with dataset_2
-    /*BST *bst2 = insertToBST(data_2,x);
-    searchBST(bst2,x,search_data);
-    deleteFromBST(bst2,delete_data,x);
-    cout << '\n';*/
-
-    // Red-Black tree with dataset_1
-    /*RBTree *rbTree1 = insertToRBTree(data_1,x);
-    searchRBTree(rbTree1, x,search_data);
-    deleteFromRBTree(rbTree1,delete_data ,x);
-    cout << '\n';*/
-
-    // Red-Black tree with dataset_2
-    /*RBTree *rbTree2 = insertToRBTree(data_2,x);
-    searchRBTree(rbTree2,x,search_data);
-    deleteFromRBTree(rbTree2,delete_data,x);
-    cout << '\n';*/
+    /*BST *bst_d2 = BSTInsertion(data_2,s);
+    BSTSearch(bst_d2,s,search_data);
+    BSTDeletion(bst_d2,delete_data,s);*/
 
     // Splay tree with dataset_1
-    /*SplayTree *splayTree1 = insertToSplayTree(data_1,x);
-    searchSplayTree(splayTree1,x,search_data);
-    deleteFromSplayTree(splayTree1,delete_data,x);
-    cout << '\n';*/
+    /*SplayTree *st_d1 = SplayTreeInsertion(data_1,s);
+    SplayTreeSearch(st_d1,s,search_data);
+    SplayTreeDeletion(st_d1,delete_data,s);*/
 
     // Splay tree with dataset_2
-    /*SplayTree *splayTree2 = insertToSplayTree(data_2,x);
-    searchSplayTree(splayTree2,x,search_data);
-    deleteFromSplayTree(splayTree2,delete_data,x);
-    cout << '\n';*/
+    /*SplayTree *st_d2 = SplayTreeInsertion(data_2,s);
+    SplayTreeSearch(st_d2,s,search_data);
+    SplayTreeDeletion(st_d2,delete_data,s);*/
 
+    // Red-Black tree with dataset_1
+    /*RBTree *rbt_d1 = RBTreeInsertion(data_1,s);
+    RBTreeSearch(rbt_d1, s,search_data);
+    RBTreeDeletion(rbt_d1,delete_data ,s);*/
 
+    // Red-Black tree with dataset_2
+    /*RBTree *rbt_d2 = RBTreeInsertion(data_2,s);
+    RBTreeSearch(rbt_d2,s,search_data);
+    RBTreeDeletion(rbt_d2,delete_data,s);*/
 
     cout << "Done !" << endl;
     return 0;
